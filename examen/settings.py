981b61ce06435ce34abd69deb6a99a158da236d6
matias12k <matiasbarraza55@gmail.com>
2023-05-07 20:49:09 -0400

    correcion

--- examen/settings.py
+++ examen/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,19 @@ WSGI_APPLICATION = 'examen.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+      #  'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': BASE_DIR / 'db.sqlite3',
+       'ENGINE': 'django.db.backends.oracle',
+       'NAME': '192.168.1.157:1521/XE',
+       'USER': 'DUOC2023',
+       'PASSWORD': '12345',
+       'TEST':{
+           'USER':'default_test',
+           'TBLSPACE':'default_test_tbl',
+           'TBLSPACE_TMP':'default_test_tbls_tmp',
+       },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
