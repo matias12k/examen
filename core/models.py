6b5542c089f06962f82a45708a628bf81898c207
eruxkatsumi <128930597+eruxkatsumi@users.noreply.github.com>
2023-05-07 23:48:04 -0400

    Update models.py
    referencia a un campo en la tabla de base de datos

--- core/models.py
+++ core/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 
-class formulario(models.Model):
+class Formulario(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField()
     telefono = models.CharField(max_length=20, blank=True)
     password = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.nombre
+    
 # Create your models here.
