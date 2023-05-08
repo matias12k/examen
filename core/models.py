f2482450f312cd5d34d055a44c08727a97fde19c
matias12k <matiasbarraza55@gmail.com>
2023-05-07 23:44:42 -0400

    tablas en la db

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class formulario(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20, blank=True)
+    password = models.CharField(max_length=50)
 # Create your models here.
