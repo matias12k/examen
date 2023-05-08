2ba4ec89b6d710f2368fbc325ed6a38a606b7eb4
eruxkatsumi <128930597+eruxkatsumi@users.noreply.github.com>
2023-05-07 23:59:26 -0400

    Update views.py
    manipulacion de un objeto de la base de datos para usar en html

--- core/views.py
+++ core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Formulario
 
-def inicio(request):
-    return render(request, 'core/inicio.html')
+
+# Create your views here.
+#def inicio(request):
+ #   return render(request, 'core/inicio.html')
 
 def registro(request):
     return render(request, 'core/registrarse.html')
-# Create your views here.
+
+def inicio(request):
+    formulario = Formulario.objects.all()  #Sentencia select * from Formulario;
+    contexto = {"usuario": formulario} #usuario es el nombre del objeto formulario de la base de datos con el que podemos llamar en HTML 
+    return render(request, 'core/inicio', contexto)
