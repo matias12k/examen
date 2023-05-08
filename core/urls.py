8549a405afcc75ab732ef04b325683240b7672e3
matias12k <matiasbarraza55@gmail.com>
2023-05-07 22:59:38 -0400

    registro creado

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='inicio'),
+    path('registro', views.registro, name='registro'),
     # otras rutas de URL aquí
 ]
--- core/views.py
+++ core/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 def inicio(request):
     return render(request, 'core/inicio.html')
 
+def registro(request):
+    return render(request, 'core/registrarse.html')
 # Create your views here.
